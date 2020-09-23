1e7738c5465f1ad8c6f19b8525944e6004c5e712
leeyupeng <leeyupeng@126.com>
2020-09-23 14:35:56 +0800

    Site updated: 2020-09-23 14:35:53

---
+++ project/algorithm/leetcode/include/leetcodetreenode.h
@@ -0,0 +1,31 @@
+#include"leetcode.h"
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+#define null 0
+TreeNode *GetTree(vector<int>& tree){
+    int n = tree.size();
+    if(n == 0){return NULL;}
+    TreeNode *root = new TreeNode(tree[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+    int index = 1;
+
+    while(!q.empty() && index<n){
+        TreeNode *node = q.front();
+        q.pop();
+        if(tree[index]!=null){node->left = new TreeNode(tree[index]);q.push(node->left);}
+        index++;
+        if(index<n){
+            if(tree[index]!=null){node->right = new TreeNode(tree[index]);q.push(node->right);}
+            index++;
+        }
+    }
+    return root;
+}
